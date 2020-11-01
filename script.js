170b10c40b2b586793b4233f54fdb4c050f14f6c
Ashraf Waqar <waqarashraf22@gmail.com>
2020-11-01 17:40:11 +0200

    getting images

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const APIKEY = "3a98eacfa2137f654f74ebd13657abfc";
 
 //getting list of movies from https://developers.themoviedb.org/3/discover/movie-discover
 const APIURL= "https://api.themoviedb.org/3/discover/movie?api_key=3a98eacfa2137f654f74ebd13657abfc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w500"
 
 async function fetchMovies(){
    const resp =  await fetch (APIURL);
    const data = await resp.json();
-   console.log(data)
+   //getting images and appending to body 
+   data.results.forEach((movie)=>{
+       const img = document.createElement("img")
+       img.src=IMGPATH + movie.poster_path
+       document.body.appendChild(img)
+   })
    return data;
 }
 fetchMovies();
